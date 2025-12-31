30fddf1c4800c628e21f64f0177d6156c1010cfe
MajidMoghadam <majid.moghadam114@yahoo.com>
2025-12-30 21:07:09 -0500

    Episode 2 completed: Added Gun with Shooting, Reloading and Recoil with Flash

--- Assets/Input/PlayerInput.cs
+++ Assets/Input/PlayerInput.cs
@@ -118,6 +118,33 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Shoot"",
+                    ""type"": ""Button"",
+                    ""id"": ""3863fa77-cdf8-435c-8356-52e7a6b101db"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Shoot Release"",
+                    ""type"": ""Button"",
+                    ""id"": ""5ab0f2de-4dca-499a-b224-5bed5bb03f36"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Reload"",
+                    ""type"": ""Button"",
+                    ""id"": ""6a38e84b-9aaf-4d68-bde3-273687b18be3"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -252,6 +279,39 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
                     ""action"": ""Look"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""7820a9bd-f4ee-402d-8653-cb9c9a2d3e2a"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Shoot"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4f6f4a11-d8f6-4ab6-8cbf-6ef8348cf44c"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": ""Press(behavior=1)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Shoot Release"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a506bcdc-69e9-4d9d-b2c3-998b8b9116b2"",
+                    ""path"": ""<Keyboard>/r"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Reload"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -263,6 +323,9 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
         m_OnFoot_Jump = m_OnFoot.FindAction("Jump", throwIfNotFound: true);
         m_OnFoot_Movement = m_OnFoot.FindAction("Movement", throwIfNotFound: true);
         m_OnFoot_Look = m_OnFoot.FindAction("Look", throwIfNotFound: true);
+        m_OnFoot_Shoot = m_OnFoot.FindAction("Shoot", throwIfNotFound: true);
+        m_OnFoot_ShootRelease = m_OnFoot.FindAction("Shoot Release", throwIfNotFound: true);
+        m_OnFoot_Reload = m_OnFoot.FindAction("Reload", throwIfNotFound: true);
     }
 
     ~@PlayerInput()
@@ -346,6 +409,9 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
     private readonly InputAction m_OnFoot_Jump;
     private readonly InputAction m_OnFoot_Movement;
     private readonly InputAction m_OnFoot_Look;
+    private readonly InputAction m_OnFoot_Shoot;
+    private readonly InputAction m_OnFoot_ShootRelease;
+    private readonly InputAction m_OnFoot_Reload;
     /// <summary>
     /// Provides access to input actions defined in input action map "OnFoot".
     /// </summary>
@@ -369,6 +435,18 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "OnFoot/Look".
         /// </summary>
         public InputAction @Look => m_Wrapper.m_OnFoot_Look;
+        /// <summary>
+        /// Provides access to the underlying input action "OnFoot/Shoot".
+        /// </summary>
+        public InputAction @Shoot => m_Wrapper.m_OnFoot_Shoot;
+        /// <summary>
+        /// Provides access to the underlying input action "OnFoot/ShootRelease".
+        /// </summary>
+        public InputAction @ShootRelease => m_Wrapper.m_OnFoot_ShootRelease;
+        /// <summary>
+        /// Provides access to the underlying input action "OnFoot/Reload".
+        /// </summary>
+        public InputAction @Reload => m_Wrapper.m_OnFoot_Reload;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -404,6 +482,15 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
             @Look.started += instance.OnLook;
             @Look.performed += instance.OnLook;
             @Look.canceled += instance.OnLook;
+            @Shoot.started += instance.OnShoot;
+            @Shoot.performed += instance.OnShoot;
+            @Shoot.canceled += instance.OnShoot;
+            @ShootRelease.started += instance.OnShootRelease;
+            @ShootRelease.performed += instance.OnShootRelease;
+            @ShootRelease.canceled += instance.OnShootRelease;
+            @Reload.started += instance.OnReload;
+            @Reload.performed += instance.OnReload;
+            @Reload.canceled += instance.OnReload;
         }
 
         /// <summary>
@@ -424,6 +511,15 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
             @Look.started -= instance.OnLook;
             @Look.performed -= instance.OnLook;
             @Look.canceled -= instance.OnLook;
+            @Shoot.started -= instance.OnShoot;
+            @Shoot.performed -= instance.OnShoot;
+            @Shoot.canceled -= instance.OnShoot;
+            @ShootRelease.started -= instance.OnShootRelease;
+            @ShootRelease.performed -= instance.OnShootRelease;
+            @ShootRelease.canceled -= instance.OnShootRelease;
+            @Reload.started -= instance.OnReload;
+            @Reload.performed -= instance.OnReload;
+            @Reload.canceled -= instance.OnReload;
         }
 
         /// <summary>
@@ -485,5 +581,26 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnLook(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Shoot" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnShoot(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Shoot Release" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnShootRelease(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Reload" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnReload(InputAction.CallbackContext context);
     }
 }
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+    // Firing & reload settings
+    public float reloadTime = 1f;     // time to reload
+    public float fireRate = 0.15f;    // delay between shots
+    public int maxSize = 20;          // magazine size
+
+    // Bullet & spawn point
+    public GameObject bullet;         // bullet prefab
+    public Transform bulletSpawnPoint;// muzzle / spawn position
+
+    // Recoil & flash
+    public float recoilDistance = 0.1f; // how far gun moves back
+    public float recoilSpeed = 15f;     // how fast recoil animates
+    public GameObject weaponFlash;      // muzzle flash prefab (point light)
+
+    // State
+    private int currentAmmo;            // current bullets in mag
+    private bool isReloading = false;   // true while reloading
+    private float nextTimeToFire = 0f;  // time when next shot allowed
+
+    // Reload animation (rotation)
+    private Quaternion initialRotation;                 // starting local rotation
+    private Vector3 initialPosition;                    // starting local position
+    private Vector3 reloadRotationOffset = new Vector3(66f, 50f, 50f); // reload tilt
+
+    void Start()
+    {
+        currentAmmo = maxSize;                           // fill magazine
+        initialRotation = transform.localRotation;       // cache rotation
+        initialPosition = transform.localPosition;       // cache position
+    }
+
+    public void Shoot()                                  // called while holding click
+    {
+        if (isReloading) return;                         // block during reload
+        if (Time.time < nextTimeToFire) return;          // respect fire rate
+
+        if (currentAmmo <= 0)                            // empty mag → reload
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        nextTimeToFire = Time.time + fireRate;           // schedule next shot
+        currentAmmo--;                                   // consume ammo
+
+        Instantiate(bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);     // spawn bullet at muzzle
+        //Instantiate(weaponFlash, bulletSpawnPoint.position, bulletSpawnPoint.rotation); // spawn brief muzzle flash
+
+        Instantiate(weaponFlash,bulletSpawnPoint.position - bulletSpawnPoint.forward * 0.03f, bulletSpawnPoint.rotation); // 3 cm inside the barrel
+
+
+
+        StopCoroutine(nameof(Recoil));                   // ensure single recoil
+        StartCoroutine(Recoil());                        // run recoil animation
+    }
+
+    IEnumerator Reload()                                 // reload animation
+    {
+        isReloading = true;                              // mark reloading
+
+        Quaternion targetRotation = Quaternion.Euler(    // tilt to reload pose
+            initialRotation.eulerAngles + reloadRotationOffset);
+
+        float halfReload = reloadTime / 2f;              // go out then back
+        float t = 0f;
+
+        while (t < halfReload)                           // initial → target
+        {
+            t += Time.deltaTime;
+            transform.localRotation = Quaternion.Slerp(initialRotation, targetRotation, t / halfReload);
+            yield return null;
+        }
+
+        t = 0f;
+        while (t < halfReload)                           // target → initial
+        {
+            t += Time.deltaTime;
+            transform.localRotation = Quaternion.Slerp(targetRotation, initialRotation, t / halfReload);
+            yield return null;
+        }
+
+        currentAmmo = maxSize;                           // refill mag
+        isReloading = false;                             // done
+    }
+
+    public void TryReload()                              // called on R
+    {
+        if (isReloading || currentAmmo == maxSize) return; // skip if busy/full
+        StartCoroutine(Reload());                        // start reload
+    }
+
+    IEnumerator Recoil()                                 // position-based recoil
+    {
+        Vector3 recoilTarget = initialPosition + new Vector3(0f, 0f, -recoilDistance); // move back along Z
+        float t = 0f;
+
+        while (t < 1f)                                   // initial → recoil
+        {
+            t += Time.deltaTime * recoilSpeed;
+            transform.localPosition = Vector3.Lerp(initialPosition, recoilTarget, t);
+            yield return null;
+        }
+
+        t = 0f;
+        while (t < 1f)                                   // recoil → initial
+        {
+            t += Time.deltaTime * recoilSpeed;
+            transform.localPosition = Vector3.Lerp(recoilTarget, initialPosition, t);
+            yield return null;
+        }
+
+        transform.localPosition = initialPosition;       // snap back to start
+    }
+}
